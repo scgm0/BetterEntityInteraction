8420a290b27a0ee968b4ac6ec4349692e725d6e0
scgm0 <2682963017@qq.com>
2024-12-10 13:52:51 +0800

    支持`1.20.0-rc`

--- BetterEntityInteraction/GameMainRayTraceForSelectionPrefix.cs
+++ BetterEntityInteraction/GameMainRayTraceForSelectionPrefix.cs
@@ -15,7 +15,7 @@ public static class GameMainRayTraceForSelectionPrefix {
 		BlockFilter bfilter = null,
 		EntityFilter efilter = null) {
 		___interesectionTester.LoadRayAndPos(ray);
-		___interesectionTester.blockSelectionTester = supplier;
+		___interesectionTester.bsTester = supplier;
 		var length = (float)ray.Length;
 		blockSelection = ___interesectionTester.GetSelectedBlock(length, bfilter);
 		var entitiesAround = supplier.GetEntitiesAround(ray.origin,
@@ -23,22 +23,26 @@ public static class GameMainRayTraceForSelectionPrefix {
 			length,
 			entity => efilter == null || efilter(entity));
 		Entity entity1 = null;
-		var num1 = double.MaxValue;
+		var num = double.MaxValue;
 		foreach (var entity2 in entitiesAround) {
-			var num2 = 0;
-			if (!entity2.IntersectsRay(ray, ___interesectionTester, out var num3, ref num2) || !(num3 < num1)) continue;
+			var selectionBoxIndex = 0;
+			if (!entity2.IntersectsRay(ray, ___interesectionTester, out var intersectionDistance, ref selectionBoxIndex) ||
+				!(intersectionDistance < num)) continue;
 			entity1 = entity2;
-			num1 = num3;
-			___entitySelTmp.SelectionBoxIndex = num2;
+			num = intersectionDistance;
+			___entitySelTmp.SelectionBoxIndex = selectionBoxIndex;
 			___entitySelTmp.Entity = entity2;
 			___entitySelTmp.Face = ___interesectionTester.hitOnBlockFace;
-			___entitySelTmp.HitPosition = ___interesectionTester.hitPosition.SubCopy(entity2.SidedPos.X, entity2.SidedPos.Y, entity2.SidedPos.Z);
+			___entitySelTmp.HitPosition =
+				___interesectionTester.hitPosition.SubCopy(entity2.SidedPos.X, entity2.SidedPos.Y, entity2.SidedPos.Z);
 			___entitySelTmp.Position = entity2.SidedPos.XYZ;
 		}
 
 		entitySelection = null;
-		if (entity1 == null)
+		if (entity1 == null) {
 			return false;
+		}
+
 		if (blockSelection != null) {
 			var position = blockSelection.Position;
 			var pos1 = new Vec3d(position.X, position.Y, position.Z).Add(blockSelection.HitPosition);
